23cc00ba7af2d2f5e72e3fc03d431742e1d6c6c5
Oluwafemijohn <femiogundipe01@gmail.com>
2021-04-14 07:05:52 +0100

    Initial Commit

---
+++ UiDesign/app/src/main/java/com/example/uidesign/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.uidesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        To remove the task bar from the view
+        supportActionBar?.hide()
+
+//      To switch the theme between light and dark mode
+        var darkModeSwitch = findViewById<Switch>(R.id.darkModeSwitch)
+        darkModeSwitch.setOnClickListener {
+            if (darkModeSwitch.isChecked ) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+
+    }
+
+}
